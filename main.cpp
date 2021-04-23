4c96a50456a26567346ebf022a94260a1ba9687f
Denis Gallegos <denis.gallegos03@gmail.com>
2021-04-23 14:08:36 -0600

    segundo

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
 	int opcion = 0; 
 	while (opcion != 4){
 		cout << "-----------MENU-----------" <<endl; 
-		cout << " 1) Eejercicio 1." <<endl; 
+		cout << " 1) Ejercicio 1." <<endl; 
 		cout << " 2) Ejercicio 2. " <<endl; 
 		cout << " 3) Ejercicio 3. " <<endl; 
 		cout << " 4) Salir. " <<endl; 
@@ -18,10 +18,19 @@ int main(int argc, char** argv) {
 		switch (opcion){
 			case 1:
 				{
-				cout << "--------DIBUJAR TABLERO--------" <<endl; 
-				cout << "Ingrese la cantidad n para filas y columnas: " <<endl; 
-				int n = 0; 
-				cin >> n; 
+				int n = 0;
+				bool v = false;
+				while (v == false){
+					cout << "--------DIBUJAR TABLERO--------" <<endl; 
+					cout << "Ingrese la cantidad n para filas y columnas: " <<endl;  
+					cin >> n; 
+					if (n <= 0){
+						v = false; 
+						cout << "Numero ingresado no valido. " <<endl; 
+					}else {
+						v = true; 
+					}
+				}//Fin del while de entrada. 
 				int color = 0; 
 				for (int c = 0; c < n; c++){
 					for (int j = 0; j < n; j++){
@@ -41,26 +50,51 @@ int main(int argc, char** argv) {
 					}
 				}
 				cout << "-------FIN DIBUJAR TABLERO-------" <<endl;
+				cout <<endl;
 				break; 
 				}
 			case 2: 
 				{
-				cout << "--------SUMATORIA--------" <<endl; 
-				cout << "Ingrese el limite de la sumatoria: " <<endl; 
-				int limite = 0; 
-				cin >> limite; 
-				int sumatoria = 0; 
+				int limite = 0;
+				bool v = false;
+				while (v == false){
+					cout << "--------SUMATORIA--------" <<endl; 
+					cout << "Ingrese el limite de la sumatoria: " <<endl; 
+					cin >> limite; 
+					if (limite <= 0){
+						v = false; 
+						cout << "Numero ingresado no valido. " <<endl; 
+					}else {
+						v = true; 
+					}
+				}//Fin del while de entrada. 
+				double sumatoria = 0; 
 				for (int c = 1; c <= limite; c++){
 					int parentesis = c - 1; 
 					int afuera = 2 * c; 
 					int operacion = parentesis * afuera; 
 					sumatoria += operacion; 
 				}
+				cout << "El resultado de la sumatoria es: " << sumatoria <<endl; 
+				cout << "-------FIN SUMATORIA-------" <<endl;
+				cout << endl; 
 				break; 
 				}
 			case 3: 
 				{
-					
+				int limite = 0;
+				bool v = false;
+				while (v == false){
+					cout << "----------------" <<endl; 
+					cout << "Ingrese el limite de la sumatoria: " <<endl; 
+					cin >> limite; 
+					if (limite <= 0){
+						v = false; 
+						cout << "Numero ingresado no valido. " <<endl; 
+					}else {
+						v = true; 
+					}
+				}//Fin del while de entrada. 
 				break; 
 				}
 		}//Fin del switch 
